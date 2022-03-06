6246650a11f48eeeecb3fce5f0ee837699260389
Ale-Pime <xorvak@gmail.com>
2022-03-05 23:55:47 -0300

    Navbat y proyecto inicial

---
+++ e-commy/src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import "../Navbar";
+
+function Navbar() {
+    <nav className="nav">
+      <a href="#" className="nav__brand">
+        herdoy
+      </a>
+      <ul>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Home
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            About
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Portfolio
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Skills
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+};
+
+export default Navbar;
